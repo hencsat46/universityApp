308dc0b6dfa36de64e8f451313f75dd88bd94959
hencsat46 <name66437@gmail.com>
2023-09-17 13:37:55 +0300

    switch from net/http to echo

--- universityServer/cmd/main.go
+++ universityServer/cmd/main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
+	app "universityServer/internal/server"
 	usecase "universityServer/internal/tools/handle"
-
-	"github.com/rs/cors"
 )
 
 func registration(w http.ResponseWriter, r *http.Request) {
@@ -47,54 +45,57 @@ func registration(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func getUniversity(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		dataMap := make(map[string]int)
+// func getUniversity(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == "POST" {
+// 		dataMap := make(map[string]int)
 
-		err := json.NewDecoder(r.Body).Decode(&dataMap)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+// 		err := json.NewDecoder(r.Body).Decode(&dataMap)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
 
-		result, err := usecase.ParseUniversityJson(dataMap["order"])
+// 		result, err := usecase.ParseUniversityJson(dataMap["order"])
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
 
-		jsonUniversity := make(map[string]string)
-		jsonUniversity["name"] = result[0]
-		jsonUniversity["description"] = result[1]
-		jsonUniversity["imagePath"] = result[2]
-		jsonUniversity["left"] = result[3]
-		convertUniversity, err := json.Marshal(jsonUniversity)
+// 		jsonUniversity := make(map[string]string)
+// 		jsonUniversity["name"] = result[0]
+// 		jsonUniversity["description"] = result[1]
+// 		jsonUniversity["imagePath"] = result[2]
+// 		jsonUniversity["left"] = result[3]
+// 		convertUniversity, err := json.Marshal(jsonUniversity)
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(convertUniversity)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 		w.Header().Set("Content-Type", "application/json")
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write(convertUniversity)
 
-	}
+// 	}
 
-}
+// }
 
 func main() {
-	r := http.NewServeMux()
+	// r := http.NewServeMux()
 
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5000"},
-		AllowCredentials: true,
-		AllowedMethods:   []string{"GET", "DELETE", "POST", "PUT"},
-	})
+	// c := cors.New(cors.Options{
+	// 	AllowedOrigins:   []string{"http://localhost:5000"},
+	// 	AllowCredentials: true,
+	// 	AllowedMethods:   []string{"GET", "DELETE", "POST", "PUT"},
+	// })
 
-	r.HandleFunc("/signup", registration)
-	r.HandleFunc("/getUniversity", getUniversity)
-	handler := c.Handler(r)
-	fmt.Println("Server is listening...")
-	log.Fatal(http.ListenAndServe(":3000", handler))
+	// r.HandleFunc("/signup", registration)
+	// r.HandleFunc("/getUniversity", getUniversity)
+	// handler := c.Handler(r)
+	// fmt.Println("Server is listening...")
+	// log.Fatal(http.ListenAndServe(":3000", handler))
+
+	app.Run()
+
 }
