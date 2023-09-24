0452b438e910014e35c2b3e4937c4d1c677b6ab8
hencsat46 <name66437@gmail.com>
2023-09-24 10:13:17 +0300

    add student done

--- universityServer/internal/database/sql/functions.sql
+++ universityServer/internal/database/sql/functions.sql
@@ -23,3 +23,16 @@ BEGIN
 	END IF;
 	RETURN -1;
 END; $$
+
+CREATE OR REPLACE FUNCTION add_record(login_name TEXT, university TEXT, points INT)
+RETURNS INT
+LANGUAGE plpgsql AS $$
+DECLARE
+	st_id INT;
+	university_id INT;
+BEGIN
+	st_id = (SELECT user_id FROM users WHERE username = login_name);
+	university_id = (SELECT uni_id FROM tempUni WHERE uni_name = university);
+	INSERT INTO students_records(student_id, student_university, student_points) VALUES (st_id, university_id, points);
+	RETURN 0;
+END; $$
